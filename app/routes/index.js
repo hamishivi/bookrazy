50c3cc845cc532581cb0a7fc6999b7a7f7467b84
Hamish Ivison <hamishivi@gmail.com>
2017-07-08 20:08:50 +1000

    Clean up indentation and remove empty files

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,43 +6,47 @@ var mongoose = require('mongoose');
 var stormpath = require('express-stormpath');
 
 
-module.exports = function (app) {
-    
-    function requireLogin (req, res, next) {
+module.exports = function(app) {
+
+    function requireLogin(req, res, next) {
         if (req.user) {
             next();
         } else {
             res.redirect('/login');
         }
     }
-    
+
     app.set('view engine', 'ejs');
-    
+
     // needs users and books
-    app.get('/', stormpath.getUser, function (req, res) {
+    app.get('/', stormpath.getUser, function(req, res) {
         res.locals.user = req.user;
         var username = "";
         if (req.user) {
             username = req.user.username;
         }
-        Book.find({creator: {$ne : username}}, function(err, data) {
+        Book.find({
+            creator: {
+                $ne: username
+            }
+        }, function(err, data) {
             if (err) return err;
             res.locals.books = data;
             res.render(path + '/public/index.ejs');
         });
     });
-    
+
     app.get('/help', stormpath.getUser, function(req, res) {
         res.locals.user = req.user;
         res.render(path + '/public/help.ejs');
     });
-    
-     app.get('/profile', stormpath.getUser, requireLogin, function (req, res) {
+
+    app.get('/profile', stormpath.getUser, requireLogin, function(req, res) {
         res.locals.user = req.user;
         //console.log(req.user);
         res.render(path + '/public/profile.ejs');
     });
-    
+
     app.get('/addFullName/:name', stormpath.getUser, requireLogin, function(req, res) {
         req.user.customData.fullName = req.params.name;
         req.user.customData.save(function(err) {
@@ -50,7 +54,7 @@ module.exports = function (app) {
             res.redirect('/profile');
         });
     });
-    
+
     app.get('/addCity/:city', stormpath.getUser, requireLogin, function(req, res) {
         req.user.customData.city = req.params.city;
         req.user.customData.save(function(err) {
@@ -59,7 +63,7 @@ module.exports = function (app) {
             res.redirect('/profile');
         });
     });
-    
+
     app.get('/addState/:state', stormpath.getUser, requireLogin, function(req, res) {
         req.user.customData.state = req.params.state;
         req.user.customData.save(function(err) {
@@ -67,22 +71,24 @@ module.exports = function (app) {
             res.redirect('/profile');
         });
     });
-    
+
     app.get('/dashboard', stormpath.getUser, requireLogin, function(req, res) {
         res.locals.user = req.user;
-        Book.find({'creator': req.user.email}, function(err, data) {
+        Book.find({
+            'creator': req.user.email
+        }, function(err, data) {
             if (err) return err;
             res.locals.books = data;
             res.render(path + '/public/dashboard.ejs');
         });
-        
+
     });
-    
+
     app.get('/addBook', stormpath.getUser, requireLogin, function(req, res) {
         res.locals.user = req.user;
         res.render(path + '/public/addBook.ejs');
     });
-    
+
     app.post('/api/addbook', stormpath.getUser, requireLogin, function(req, res) {
         //console.log(req.body);
         var newBook = new Book();
@@ -95,17 +101,19 @@ module.exports = function (app) {
             res.redirect('/');
         });
     });
-    
+
     app.get('/trade/:bookid/', stormpath.getUser, requireLogin, function(req, res) {
         Book.findById(req.params.bookid, function(err, book) {
             if (err) console.log(err);
-            if (book == null) { 
+            if (book == null) {
                 console.log("Book was null when trading. please check me!!");
                 res.redirect('/');
                 return;
             }
             // need to add book to pending trades of user who owns it
-            app.get('stormpathApplication').getAccounts( { username: book.creator }, function(err, accounts) { 
+            app.get('stormpathApplication').getAccounts({
+                username: book.creator
+            }, function(err, accounts) {
                 if (err) return err;
                 accounts.each(function(account, cb) {
                     account.getCustomData(function(err, customData) {
@@ -117,7 +125,7 @@ module.exports = function (app) {
                         //console.log(book)
                         customData.pendingTrades.push(book);
                         book.remove();
-                        customData.save(function(err){
+                        customData.save(function(err) {
                             if (err) return err;
                             // add some kind of success message?
                             res.redirect("/");
@@ -127,7 +135,7 @@ module.exports = function (app) {
             });
         });
     });
-    
+
     app.get('/stopTrade/:bookid', stormpath.getUser, requireLogin, function(req, res) {
         var trades = req.user.customData.pendingTrades;
         for (var i = 0; i < trades.length; i++) {
@@ -145,7 +153,7 @@ module.exports = function (app) {
             }
         }
     });
-    
+
     app.get('/accept/:bookid', stormpath.getUser, requireLogin, function(req, res) {
         var trades = req.user.customData.pendingTrades;
         for (var i = 0; i < trades.length; i++) {
@@ -153,7 +161,9 @@ module.exports = function (app) {
             if (book._id == req.params.bookid) {
                 res.locals.email = book.wanter;
                 res.locals.id = book._id;
-                app.get('stormpathApplication').getAccounts( { username: book.wanter }, function(err, accounts) { 
+                app.get('stormpathApplication').getAccounts({
+                    username: book.wanter
+                }, function(err, accounts) {
                     if (err) return err;
                     accounts.each(function(account, cb) {
                         account.getCustomData(function(err, customData) {
@@ -164,7 +174,7 @@ module.exports = function (app) {
                             customData.receiving.push(book);
                             req.user.customData.pendingTrades.splice(trades.indexOf(book), 1); //remove book from pending trades and place in recieving
                             req.user.customData.save();
-                            customData.save(function(err){
+                            customData.save(function(err) {
                                 if (err) return err;
                                 // accept and send email
                                 res.render(path + "/public/accept.ejs");
@@ -175,9 +185,11 @@ module.exports = function (app) {
             }
         }
     });
-    
+
     app.get('/cancelTrade/:bookid/:email', stormpath.getUser, requireLogin, function(req, res) {
-        app.get('stormpathApplication').getAccounts( { username: req.params.email }, function(err, accounts) { 
+        app.get('stormpathApplication').getAccounts({
+            username: req.params.email
+        }, function(err, accounts) {
             if (err) return err;
             accounts.each(function(account, cb) {
                 account.getCustomData(function(err, customData) {
@@ -201,7 +213,7 @@ module.exports = function (app) {
             });
         });
     });
-    
+
     // book gets swapped to new owner
     app.get('/received/:bookid', stormpath.getUser, requireLogin, function(req, res) {
         for (var i = 0; i < req.user.customData.receiving.length; i++) {
@@ -220,7 +232,7 @@ module.exports = function (app) {
             }
         }
     });
-    
+
     // book goes back to its original owner
     app.get('/denied/:bookid', stormpath.getUser, requireLogin, function(req, res) {
         for (var i = 0; i < req.user.customData.receiving.length; i++) {
@@ -235,13 +247,16 @@ module.exports = function (app) {
                     if (err) return err;
                     res.redirect('/');
                 });
-                
+
             }
         }
     });
 
     app.get('/delete/:bookid/', stormpath.getUser, requireLogin, function(req, res) {
-        Book.findOneAndRemove({"_id":req.params.bookid, "creator": req.user.email}, function(err, data) {
+        Book.findOneAndRemove({
+            "_id": req.params.bookid,
+            "creator": req.user.email
+        }, function(err, data) {
             if (err) return err;
             // again add success message? its pretty fast and clear what happens...
             res.redirect('/dashboard');
@@ -256,6 +271,6 @@ module.exports = function (app) {
 };
 
 String.prototype.toObjectId = function() {
-  var ObjectId = (require('mongoose').Types.ObjectId);
-  return new ObjectId(this.toString());
+    var ObjectId = (require('mongoose').Types.ObjectId);
+    return new ObjectId(this.toString());
 };
--- server.js
+++ server.js
@@ -12,45 +12,49 @@ var app = express();
 require('dotenv').load();
 
 
-mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
+mongoose.connect(process.env.MONGO_URI, {
+    useMongoClient: true
+});
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(session({
-  cookieName: 'session',
-  secret: 'eg[isfd-8yF9-7w2315df{}+Ijsli;;to8',
-  duration: 30 * 60 * 1000,
-  activeDuration: 5 * 60 * 1000,
-  httpOnly: true,
-  secure: true,
-  ephemeral: true
+    cookieName: 'session',
+    secret: 'eg[isfd-8yF9-7w2315df{}+Ijsli;;to8',
+    duration: 30 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    ephemeral: true
 }));
 
 app.use(stormpath.init(app, {
-   expand: {
-    customData: true,
-  }
+    expand: {
+        customData: true,
+    }
 }));
 
 // Enable CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.set('view engine', 'ejs');
 
-app.on('stormpath.ready', function () {
-  console.log('Stormpath Ready!');
+app.on('stormpath.ready', function() {
+    console.log('Stormpath Ready!');
 });
 
 routes(app);
 
 var port = process.env.PORT || 8080;
-app.listen(port, function () {
-   console.log('Node.js listening on port ' + port + '...');
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port + '...');
 });
